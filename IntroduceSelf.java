c0655658c513d56b91470bc40b83500bad42ef5b
MrRyzs <109296752+MrRyzs@users.noreply.github.com>
2022-07-14 21:57:51 +0700

    Add files via upload

---
+++ IntroduceSelf.java
@@ -0,0 +1,11 @@
+public class IntroduceSelf{
+	public static void main(String[] args) {
+		String lastName = "Tamam";
+		String fullName = "Mufid Nur Tamam";
+		int age = 15;
+		
+		System.out.println("My Name is " + fullName);
+		System.out.println("You can call me " + lastName);
+		System.out.println("And my age is " + age);
+	}
+}
